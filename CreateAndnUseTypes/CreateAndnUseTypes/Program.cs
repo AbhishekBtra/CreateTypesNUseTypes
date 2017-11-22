13cc0c06a9a28be47e7cfa79d46b28574e06db52
JustGitt <abatra002@gmail.com>
2017-11-22 18:38:06 +0530

    Commit

---
+++ CreateAndnUseTypes/CreateAndnUseTypes/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAndnUseTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program p = new Program();
+            //p.OnlyCallStructFns(p);
+
+            Days d = Days.Sunday;
+            int d_int = (int)Days.Sunday + 9;
+            Console.WriteLine("{0}    {1}",d,d_int);
+
+            Console.WriteLine("First day is {0} i.e {1}",(int)Days.Sunday,Days.Sunday);
+            
+            Console.WriteLine("Fifthday is {0} i.e {1}", 5, (Days)5);
+            Console.ReadKey();
+        }
+
+        /*
+         * An enumeration type provides an efficient way to define a set of 
+         * NAMED INTEGRAL CONSTANTS
+         * that may be assigned to a variable
+         */
+        public enum Days //Every enumeration type has an underlying type(default int), which can be any integral type except char.
+        {
+           Sunday ,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday
+        }
+
+        public enum EvenNUmLessThan10  : uint
+        {
+            //2,4,6,8 error 
+            //"2","2","2" error
+            ab,cd,ef,gh
+
+            
+        }
+
+
+
+
+        public void OnlyCallStructFns(Program p)
+        {
+            p.AccessAll();
+            p.CheckEquivalence();
+            p.CopiedByBalue();
+        }
+        public void AccessAll()
+        {
+
+            /*Working with empty struct  - start*/
+            IAmEmpty i;// initialized w/o new operator , i variable on stack , by default the default 0 param contructor is called
+            Console.WriteLine("HashCode of i ={0}",i.GetHashCode());
+            /*Working with empty struct -  end*/
+
+
+            #region NonEmptyStruct
+            /*Working with non empty struct  - start*/
+            Clock c;
+            c.hours = 5;
+            c.min = 10;
+            //c.seconds = 14;
+
+
+            //Console.WriteLine("hours = {0} min = {1} sec ={2}", c.hours, c.min, c.seconds);
+            //Console.WriteLine("structs can be initialized w/o NEW operator - hash = {0}",c.GetHashCode()); 
+            //these above 2 stmts are compiler error coz all fields of struct are not assigned.
+
+
+            Clock c1;
+            c1.hours = 5;
+            c1.min = 10;
+            c1.seconds = 14;
+
+            Console.WriteLine("hours = {0} min = {1} sec ={2}", c1.hours, c1.min, c1.seconds);
+            Console.WriteLine("structs can be initialized w/o NEW operator - hash = {0}", c1.GetHashCode());
+            /*Working with non empty struct  - end*/
+            #endregion
+
+
+        }
+
+        public void CheckEquivalence()
+        {
+            #region nonempty
+            Console.WriteLine("\n I check quivalence of value types");
+
+            Clock c;
+            c.hours = 10;
+            c.min = 5;
+            c.seconds = 54;
+            Clock c1;
+            c1.hours = 9;
+            c1.min = 5;
+            c1.seconds = 54;
+
+            if (c.Equals(c1))
+                Console.WriteLine("Equal");
+            else
+            Console.WriteLine("Not Equal");//Answer - coz all fields dont match i.e. hours are different
+            #endregion
+
+            #region empty
+            IAmEmpty i,j;
+            if (i.Equals(j))
+                Console.WriteLine("Equal"); //Anser - coz empty with no fields
+            else
+            Console.WriteLine("Not Equal");
+
+            #endregion
+        }
+
+        public void CopiedByBalue()
+        {
+            Clock c1;
+            c1.hours = 5;
+            c1.min = 10;
+            c1.seconds = 14;
+
+            Clock c2 = c1;
+
+            c2.hours = 21;
+            Console.WriteLine("C1.hours ={0} and c2.hours = {1}",c1.hours,c2.hours); //ans - C1.hours = 5 and c2.hours = 21 coz both have their own copy
+        }
+    }
+}
---
+++ CreateAndnUseTypes/CreateAndnUseTypes/ValueTypes_structs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAndnUseTypes
+{
+    public struct Clock
+    {
+
+        public int hours;
+        public int min;
+        public int seconds;
+
+
+        #region PartialInitializingConstructor
+        //public Clock(int hrs, int min)
+        //{
+        //    this.hours = hrs;
+        //    this.min = min;
+        //   // This contructor is compiler error coz all the fields of struct are not initialized in the constructor 
+        //} 
+        #endregion
+
+
+        #region AllFieldsInitializationConstructor
+        public Clock(int hrs, int min, int sec)
+        {//proper definition of contructor, i.e. containing initialization all the fields
+         //if we do not explicitlty add the constructor then by default there is non parameterized constructor
+         //NOTE - structs allow parameterized contructors to be added explicitly.
+            this.hours = hrs;
+            this.min = min;
+            this.seconds = sec;
+        } 
+        #endregion
+
+    }
+
+    public struct IAmEmpty
+    {
+        // I ony  have default contructor.
+        
+    }
+
+    
+
+
+}
